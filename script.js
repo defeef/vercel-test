387562b6e33a7a85c427fb8223a03ef1b7b40b1e
defeef <henrybuild@roadrunner.com>
2022-10-03 21:03:01 -0400

    Tests

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ window.onload = function() {
   console.log("Test");
   let btn = document.createElement("button");
   btn.onclick = function() {
+    console.log("Button Start")
     var audio = new Audio("jumpscare.mp3");
     audio.play();
+    console.log("Button End")
   }
   btn.innerHTML = "Click Me";
   document.body.appendChild(btn);
