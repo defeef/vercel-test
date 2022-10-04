651802ed529f1cdcbe178d5168de259793bb4509
defeef <henrybuild@roadrunner.com>
2022-10-04 16:29:11 -0400

    Try again

--- script.js
+++ script.js
@@ -12,14 +12,18 @@ window.onload = function() {
   button_holder.appendChild(btn);
   console.log(document.getElementsByClassName("button-holder")[0]);
   
-  let btn2 = document.createElement("button");
+  let btn2 = document.createElement("button2");
   btn2.onclick = function() {
     console.log("Button Start")
     // Say stuff
+    var msg = new SpeechSynthesisUtterance();
+    msg.text = "Hello World";
+    window.speechSynthesis.speak(msg);
+    //
     console.log("Button End")
   }
   btn2.innerHTML = "Click Me Too";
-  btn2.id = "btn2"
+  //btn2.id = "btn2"
   button_holder.appendChild(btn);
   console.log(document.getElementsByClassName("button-holder")[0]);
 }
